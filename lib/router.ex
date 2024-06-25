2382fcf52435ed3bd3b9d7357cffc0931580286f
j <email@gmail.com>
2024-06-25 17:27:39 +0200

    make code robust against invalid routes

--- lib/router.ex
+++ lib/router.ex
@@ -6,24 +6,24 @@ defmodule Router do
     Process.put(:router, bucket)
   end
 
-  def add_route(path, callback_function) when is_binary(path) do
-    # get existing routes config
-    routes_config = Router.Bucket.get(Process.get(:router))
+  def add_route(path, callback_function) do
+    if(is_binary(path)) do
+      # get existing routes config
+      routes_config = Router.Bucket.get(Process.get(:router))
 
-    # split route into keys
-    new_route_keys = String.split(path, "/")
+      # split route into keys
+      new_route_keys = String.split(path, "/")
 
-    # generate new route config with the new route
-    routes_config = build_routes_config(new_route_keys, routes_config, callback_function)
+      # generate new route config with the new route
+      routes_config = build_routes_config(new_route_keys, routes_config, callback_function)
 
-    # Update the config map at Router Bucket
-    Router.Bucket.set(Process.get(:router), routes_config)
+      # Update the config map at Router Bucket
+      Router.Bucket.set(Process.get(:router), routes_config)
 
-    IO.puts("Added route: #{path}")
-  end
-
-  def add_route(_, _) do
-    raise ArgumentError, message: "Input must be a string"
+      IO.puts("Added route: #{path}")
+    else
+      IO.puts("Invalid route path #{path}")
+    end
   end
 
   def get_route_callback(path) do
@@ -36,14 +36,14 @@ defmodule Router do
   end
 
   defp read_routes_config([], acc, data) do
-    read_config_nokey(nil, [] , acc, data)
+    read_config_nokey(nil, [], acc, data)
   end
-
 
   defp read_routes_config([key | keys], acc, data) do
     case Map.get(acc, key) do
       nil ->
-        read_config_nokey(key, keys , acc, data)
+        read_config_nokey(key, keys, acc, data)
+
       map ->
         read_routes_config(keys, map, data)
     end
@@ -55,6 +55,7 @@ defmodule Router do
         case Map.get(acc, :default) do
           value when is_function(value) ->
             Map.put(data, :callback, value)
+
           _ ->
             data
         end
@@ -65,6 +66,7 @@ defmodule Router do
         case Map.get(acc, :default) do
           value when is_function(value) ->
             Map.put(map_with_dynamic, :callback, value)
+
           _ ->
             Map.put(map_with_dynamic, :dynamic, [key] ++ Map.get(data, :dynamic))
         end
@@ -97,5 +99,4 @@ defmodule Router do
       trimmed_key
     end
   end
-
 end
